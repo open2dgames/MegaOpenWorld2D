4e1520baadb36580206edcaabd002a86cda20f92
open2dgames <opensourcewater@outlook.com>
2024-09-18 21:47:32 +0200

    Add the project made from the tutorial of Graven #1

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+from game import Game
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.run()
+
+# Reprendre le TUto de Graven sur faire un jeu en Python à 48 minutes
