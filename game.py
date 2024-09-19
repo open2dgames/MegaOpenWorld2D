6304c1fe84de1293c7432ef6e939ff442a121db5
unknown <opensourcewater@outlook.com>
2024-09-19 07:03:38 +0200

    adding passing world capabilities

--- game.py
+++ game.py
@@ -31,6 +31,13 @@ class Game:
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4) # ICI LE DEFAULT LAYER
         self.group.add(self.player)
 
+        # definir le rect de collision pour entrer dans la maison
+        enter_zone = tmx_data.get_object_by_name('enter_zone')
+        self.enter_zone_rect = pygame.Rect(enter_zone.x, enter_zone.y, enter_zone.width, enter_zone.height)
+
+        # definir le monde où se trouve le joueur
+        self.map = 'world'
+
     def handle_input(self):
         pressed = pygame.key.get_pressed()
 
@@ -50,8 +57,72 @@ class Game:
             self.player.move_left()
             self.player.change_animation('left')
 
+    def switch_zone(self):
+        # charger la zone (tmx)
+        tmx_data = pytmx.util_pygame.load_pygame('zone1.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 1
+
+        # definir une liste qui va stocker les rectangles de collision
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # dessiner le groupe de calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4) # ICI LE DEFAULT LAYER
+        self.group.add(self.player)
+
+        # definir le rect de collision pour entrer dans la maison
+        enter_zone = tmx_data.get_object_by_name('exit_zone') # Ici bien changé le enter_zone pour que le joueur ne reste pas bloquer dans ce monde éternellement
+        self.enter_zone_rect = pygame.Rect(enter_zone.x, enter_zone.y, enter_zone.width, enter_zone.height)
+
+        # récuperer le point de spawn devant la maison
+        spawn_house_point = tmx_data.get_object_by_name('spawn_zone')
+        self.player.position[0] = spawn_house_point.x
+        self.player.position[1] = spawn_house_point.y
+
+    def switch_world(self):
+                # charger la carte (tmx)
+        tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 1
+
+        # definir une liste qui va stocker les rectangles de collision
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # dessiner le groupe de calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4) # ICI LE DEFAULT LAYER
+        self.group.add(self.player)
+
+        # definir le rect de collision pour entrer dans la maison
+        enter_zone = tmx_data.get_object_by_name('enter_zone') # Ici bien changé le enter_zone pour que le joueur ne reste pas bloquer dans ce monde éternellement
+        self.enter_zone_rect = pygame.Rect(enter_zone.x, enter_zone.y, enter_zone.width, enter_zone.height)
+
+        # récuperer le point de spawn devant la maison
+        spawn_house_point = tmx_data.get_object_by_name('enter_zone_exit')
+        self.player.position[0] = spawn_house_point.x
+        self.player.position[1] = spawn_house_point.y
+
     def update(self):
         self.group.update()
+
+        # vérifier l'entrée dans la zone 1
+        if  self.map == 'world' and self.player.feet.colliderect(self.enter_zone_rect):
+            self.switch_zone()
+            self.map = 'zone1'
+
+        # vérifier l'entrer dans le monde 
+        if self.map == 'zone1' and self.player.feet.colliderect(self.enter_zone_rect):
+            self.switch_world()
+            self.map = 'world'
 
         # verification collision
         for sprite in self.group.sprites():
