b51f6bb4cc58d39a7099cbc58f4cb644ecaa7f02
unknown <opensourcewater@outlook.com>
2024-09-23 21:31:59 +0200

    readme update

--- src/main.py
+++ src/main.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
     game = Game()
     game.run()
 
-# Road to finish part 2 du tutoriel de Graven
-# - les animations, passage derrière les arbres, dialogues
 
 # Pouvoir ajouter des maps sans passer par le code
 # Faire une documentation en béton pour les beginners
+
 # Changer le code pour que les portails fonctionnent sans référencement dans le code (avec des boucles objects et des names spécifiques)
 # pareil pour les npcs (et leurs déplacement sans référencement) (pour rendre le process rapide et faisable directement dans Tiled)
 # Régler le système de mouvement du joueur (diag)  
